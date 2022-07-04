bdbd54b5d6f722137af23597ae524c0d51fe40d9
Junaid Arshad <jun.arshad@gmail.com>
2022-07-04 14:08:05 -0400

    Change ServiceProxy object creation to function "connect_to_cucm()". Call this function in the main function and pass that ServiceProxy object to "add_app_user" function.

--- main.py
+++ main.py
@@ -26,6 +26,36 @@ class MyLoggingPlugin(Plugin):
         print(f'\nResponse\n-------\nHeaders:\n{http_headers}\n\nBody:\n{xml}')
 
 
+def connect_to_cucm(username: str, password: str) -> Client.service:
+    # The WSDL is a local file which contains the CUCM Schema
+    WSDL_FILE = 'schema/AXLAPI.wsdl'
+    CUCM_ADDRESS = '172.25.116.110'
+
+    # Change to true to enable output of request/response headers and XML
+    DEBUG = False
+
+    session = Session()
+    session.verify = False
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    session.auth = HTTPBasicAuth(username, password)
+
+    # Create a Zeep transport and set a reasonable timeout value
+    transport = Transport(session=session, timeout=10)
+
+    # strict=False is not always necessary, but it allows zeep to parse imperfect XML
+    settings = Settings(strict=False, xml_huge_tree=True)
+
+    # If debug output is requested, add the MyLoggingPlugin callback
+    plugin = [MyLoggingPlugin()] if DEBUG else []
+
+    # Create the Zeep client with the specified settings
+    client = Client(WSDL_FILE, settings=settings, transport=transport, plugins=plugin)
+
+    # Return the ServiceProxy object
+    return client.create_service('{http://www.cisco.com/AXLAPIService/}AXLAPIBinding',
+                                 f'https://{CUCM_ADDRESS}:8443/axl/')
+
+
 # Function to create a test application User
 def add_app_user(uc, userid: str, password: str, presence_group_name: str):
     app_user = {
@@ -41,7 +71,7 @@ def add_app_user(uc, userid: str, password: str, presence_group_name: str):
         print("\naddAppUser response:\n")
         print(resp, "\n")
     except Exception as err:
-        print("\nZeep error: addAppUser: {0}".format(err))
+        print(f"\nError: addAppUser: {err}")
 
 
 def cls():
@@ -49,43 +79,21 @@ def cls():
 
 
 if __name__ == '__main__':
-    # The WSDL is a local file which contains the CUCM Schema
-    WSDL_FILE = 'schema/AXLAPI.wsdl'
-    CUCM_ADDRESS = '172.25.116.110'
+    running = True
+    cucm = connect_to_cucm(input("Username: "), getpass())
 
-    # Change to true to enable output of request/response headers and XML
-    DEBUG = False
-
-    session = Session()
-    session.verify = False
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    session.auth = HTTPBasicAuth(input("Username: "), getpass())
-
-    # Create a Zeep transport and set a reasonable timeout value
-    transport = Transport(session=session, timeout=10)
-
-    # strict=False is not always necessary, but it allows zeep to parse imperfect XML
-    settings = Settings(strict=False, xml_huge_tree=True)
-
-    # If debug output is requested, add the MyLoggingPlugin callback
-    plugin = [MyLoggingPlugin()] if DEBUG else []
-
-    # Create the Zeep client with the specified settings
-    client = Client(WSDL_FILE, settings=settings, transport=transport, plugins=plugin)
-
-    service = client.create_service('{http://www.cisco.com/AXLAPIService/}AXLAPIBinding',
-                                    f'https://{CUCM_ADDRESS}:8443/axl/')
-
-    while True:
+    while running:
         cls()
         print("\nWhat would you like to do?\n(1) Create an Application User")
         option = input('\nEnter a number (or type Q to quit): ')
         if option.lower() == 'q':
             input('\nYou have Quit.\nPress Enter to exit...')
-            break
+            cls()
+            running = False
         elif option == '1':
             # Test calling the add app user function
-            add_app_user(service, 'testAppUser', 'Cisco1234', 'Standard Presence Group', )
-            input('Press Enter to continue...')
+            add_app_user(cucm, input("New Application User's Username: "), 'Cisco1234', 'Standard Presence Group', )
         else:
             print("Invalid entry...")
+
+        input('Press Enter to continue...')
